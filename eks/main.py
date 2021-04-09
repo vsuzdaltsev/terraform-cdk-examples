9c461a33f4a879c4aaf70f2bec885952e414f0a0
vsuzdaltsev <suzdaltsev_v@mail.ru>
2021-04-09 13:39:10 +0300

    extract constants

--- eks/main.py
+++ eks/main.py
@@ -13,7 +13,22 @@ from imports.eks import Eks
 
 
 AWS_REGION = 'us-west-2'
+AVAILABILITY_ZONES = [
+    f"{AWS_REGION}a",
+    f"{AWS_REGION}b",
+    f"{AWS_REGION}a"]
 TAGS = {"Environment": "test", "Delete me": "true"}
+VPC_CIDR = '10.0.0.0/16'
+PRIVATE_SUBNETS = [
+    '10.0.1.0/24',
+    '10.0.2.0/24',
+    '10.0.3.0/24']
+PUBLIC_SUBNETS = [
+    '10.0.101.0/24',
+    '10.0.102.0/24',
+    '10.0.103.0/24']
+CLUSTER_VERSION = '1.17'
+NODE_GROUP_INSTANCE_TYPE = "m5.large"
 
 
 class BasicStack(TerraformStack):
@@ -24,34 +39,25 @@ class BasicStack(TerraformStack):
 
         eks_vpc = Vpc(
             self, 'TestEksClusterVpc',
-            name='my-vpc',
-            cidr='10.0.0.0/16',
-            azs=[
-                f"{AWS_REGION}a",
-                f"{AWS_REGION}b",
-                f"{AWS_REGION}a"],
-            private_subnets=[
-                '10.0.1.0/24',
-                '10.0.2.0/24',
-                '10.0.3.0/24'],
-            public_subnets=[
-                '10.0.101.0/24',
-                '10.0.102.0/24',
-                '10.0.103.0/24'],
+            name='test-eks-cluster-vpc',
+            cidr=VPC_CIDR,
+            azs=AVAILABILITY_ZONES,
+            private_subnets=PRIVATE_SUBNETS,
+            public_subnets=PUBLIC_SUBNETS,
             enable_nat_gateway=True,
             tags=TAGS
         )
 
-        my_eks = Eks(
-            self, 'TestEks',
-            cluster_name='my-eks',
+        eks_cluster = Eks(
+            self, 'TestEksCluster',
+            cluster_name='test-eks-cluster',
             subnets=Token().as_list(eks_vpc.private_subnets_output),
             vpc_id=Token().as_string(eks_vpc.vpc_id_output),
             manage_aws_auth=False,
-            cluster_version='1.17',
+            cluster_version=CLUSTER_VERSION,
             node_groups=[
                 {
-                    "instance_types": ["m5.large"],
+                    "instance_types": [NODE_GROUP_INSTANCE_TYPE],
                     "capacity_type":"SPOT"
                 }
             ],
@@ -60,7 +66,7 @@ class BasicStack(TerraformStack):
 
         TerraformOutput(
             self, 'cluster_endpoint',
-            value=my_eks.cluster_endpoint_output
+            value=eks_cluster.cluster_endpoint_output
         )
 
         TerraformOutput(
